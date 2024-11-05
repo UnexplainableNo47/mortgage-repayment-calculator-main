f6bfe49697807686234016a3f80e981a1ecb4f00
Mart <78298042+UnexplainableNo47@users.noreply.github.com>
2024-11-05 19:39:17 +0200

    clear not working

--- index.js
+++ index.js
@@ -1,48 +1,99 @@
 const form = document.getElementById('form')
 
+const Static_Panel = document.getElementById('Static-Panel')
+const Results_Panel = document.getElementById('Results-Panel')
+
 const mortgage_amount = document.getElementById('mortgage-Amount')
 const mortgage_Term = document.getElementById('mortgage-Term')
 const interest_Rate = document.getElementById('Interest-Rate')
 
+const repayment = document.getElementById('Repayment')
+const interest_Only = document.getElementById('Interest-Only')
+const checkboxes = document.querySelector('input[type="radio"]')
+
 const mortgage_Amount_Str = document.getElementById('mortgage-Amount-Msg')
 const mortgage_Term_Str = document.getElementById('mortgage-Term-Msg')
 const interest_Rate_Str = document.getElementById('Interest-Rate-Msg')
+const mortgage_Type_Str = document.getElementById('mortgage-Type-Msg')
+
+const monthly_Payments = document.getElementById('monthly-Payments')
+const repayTerm = document.getElementById('repayTerm')
+
 
 const allInputs = [mortgage_amount, mortgage_Term, interest_Rate]
 
-
+    function clear(){
+        document.getElementById("form").reset();
+    }
 
 form.addEventListener('submit', (e) =>{
     //creating error list
     let errors = []
 
-        //check if day input has been added
-    errors = getAgeCalculatorErrors(mortgage_amount.value, mortgage_Term.value, interest_Rate.value)
+    //change format of output
+    let format = new Intl.NumberFormat('en-SA',{
+        style: 'currency',
+        currency: 'ZAR'
+    })
+
+    errors = getErrors(mortgage_amount.value, mortgage_Term.value, interest_Rate.value,repayment.value,interest_Only.value)
+    m = getMonthlyPayment(mortgage_amount.value, mortgage_Term.value, interest_Rate.value)
+    pmv = getTotalPayment(m, mortgage_Term.value)
     
     if(errors.length > 0){ 
         e.preventDefault()
-    }
-    else{
         
-    
+    }
+    else{   
+    monthly_Payments.innerText = format.format(m)
+    repayTerm.innerText = format.format(pmv)
+    Static_Panel.style.display ="none"
+    Results_Panel.style.display ="block"
     }
 })
 
-function getAgeCalculatorErrors(amount,Term,Rate){
+//This code will calculate the monthly mortgage payment
+function getMonthlyPayment(mortgage_Amount,mortgage_Term,interest_Rate){
+    var p = mortgage_Amount
+    var i = (interest_Rate /100) / 12
+    var n = mortgage_Term * 12
+
+    return  p * (i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1));
+}
+function getTotalPayment(m, mortgage_Term){
+    var n = mortgage_Term * 12
+    
+    return  pmv = m * n 
+
+}
+
+//This code will find errors in the program
+function getErrors(amount,Term,Rate,){
     let errors = []
 
     if( amount === '' || amount == null){
         errors.push('this field is required')
+        mortgage_amount.classList.add('error')
         mortgage_Amount_Str.innerText = 'this field is required'
     }
     if( Term === '' || Term == null){
-        errors.push('this field is required')  
+        errors.push('this field is required')
+        mortgage_Term.classList.add('error')  
         mortgage_Term_Str.innerText = 'this field is required'
     }
     if( Rate === '' || Rate == null){
         errors.push('this field is required')
+        interest_Rate.classList.add('error')
         interest_Rate_Str.innerText = 'this field is required' 
     }
+    if(!repayment.checked && !interest_Only.checked) {
+        errors.push('this field is required')
+        repayment.classList.add('error')
+        mortgage_Type_Str.innerText = 'this field is required'
+      }else{
+        mortgage_Type_Str.innerText = ''
+      }
+        
 
     return errors;
 }
@@ -54,9 +105,13 @@ allInputs.forEach(input => {
             mortgage_Amount_Str.innerText = ''
             mortgage_Term_Str.innerText = ''
             interest_Rate_Str.innerText = ''
+            mortgage_Type_Str.innerText = ''
         }
     })
 })
+
+//This code will clear all inputs
+
 
 //This code prevents letters and special characters
 function numbersOnly(interest_Rate,mortgage_Term){
